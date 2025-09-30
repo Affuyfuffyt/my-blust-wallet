8e6690663715dbf692b9c144f559ba675d078eee
Affuyfuffyt <98175855+Affuyfuffyt@users.noreply.github.com>
2025-09-30 12:12:17 +0300

    Delete src/app/src directory

--- src/app/src/src/components/logo.tsx
+++
@@ -1,9 +0,0 @@
-import { Wallet } from 'lucide-react';
-
-export default function Logo() {
-  return (
-    <div className="flex items-center gap-3 text-primary">
-      <Wallet className="h-8 w-8" />
-    </div>
-  );
-}
